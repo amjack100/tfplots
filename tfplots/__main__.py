44bb6a74ba17fd5cb65ada1437cec25e483e3132
Andy Jackson <amjack100@gmail.com>
2021-04-21 14:00:18 -0400

    clean altair plot

--- tfplots/__main__.py
+++ tfplots/__main__.py
@@ -5,38 +5,61 @@ import pandas as pd
 import altair as alt
 from altair_saver import save
 
+TITLE_DATE = "%d-%m-%y %H:%M:%S"
+FILE_DATE = "%d-%m-%y_%H:%M:%S"
+date = datetime.datetime.now().strftime
 
-def plot_history(history, filename=None, backend='matplotlib'):
+title = lambda hist: f"Training session {date(TITLE_DATE)} - {hist.epoch[-1] + 1} epochs"
 
-    if backend == 'altair':
+
+def plot_history(history, filename=None, backend="matplotlib"):
+    """
+    Dispatch to a specific history plotter based on chosen backends
+    Backend options are matplotlib and altair
+    """
+
+    if backend == "altair":
         _plot_history_altair(history, filename)
-        return 
+        return
+    elif backend == "matplotlib":
+        _plot_history_matplotlib(history, filename)
+        return
+    else:
+        raise NotImplementedError("Available backends are matplotlib and altair")
 
-    date = datetime.datetime.now().strftime("%d-%m-%y-%H:%M:%S")
-    title = f"Training session {date} for {history.epoch[-1] + 1} epochs"
+
+def _plot_history_matplotlib(history, filename=None):
 
     if filename is None:
-        filename = date
+        filename = date(FILE_DATE)
 
     for key, value in history.history.items():
 
         plt.plot(value, label=key)
 
-    plt.title(title)
+    plt.title(title(history))
     plt.legend()
     plt.savefig(filename)
 
 
 def _plot_history_altair(history, filename=None):
 
+    if filename is None:
+        filename = date(FILE_DATE) + ".png"
+
     data = history.history
-    data['epoch'] = history.epoch
-    data = pd.DataFrame(data)
-    print(data)
-    data = data.melt('epoch')
-    print(data)
-    chart = alt.Chart(data).mark_line().encode(x="epoch", y="value", color="variable")
-    with open("./chart.png", "wb") as f:
+    data["epoch"] = history.epoch
+    data = pd.DataFrame(data).melt("epoch")
+    chart = (
+        alt.Chart(data)
+        .mark_line()
+        .encode(x=alt.X('epoch', title='Epoch'), y=alt.Y('value', title='Value'), color="variable")
+        .properties(title=title(history))
+        .configure_legend(
+            title=None
+        )
+    )
+    with open(filename, "wb") as f:
         save(chart, f)
 
 
